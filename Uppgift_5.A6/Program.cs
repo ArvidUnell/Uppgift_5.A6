7a70fe13be3cbd6d5b98ce485a49e1592fe8b26f
ArvidUnell <arun06001@utb.vaxjo.se>
2023-11-27 19:02:52 +0100

    Add project files.

---
+++ Uppgift_5.A6/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace Uppgift_5_A6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in 5 tal separerade med asterisker");
+            float produkt = 1;
+            while (true)
+            {
+                try
+                {
+                    string problem = Console.ReadLine();
+                    string[] faktorerStr = problem.Split('*');
+
+                    float[] faktorer = new float[faktorerStr.Length];
+                    for (int i = 0; i < faktorerStr.Length;i++)
+                    {
+                        faktorer[i] = float.Parse(faktorerStr[i]);
+                    }
+
+                    foreach (float tal in faktorer)
+                    {
+                        produkt *= tal;
+                    }
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Det är inte ett giltigt problem! Försök igen.");
+                }
+            }
+
+            Console.WriteLine($"Produkt: {produkt}.");
+
+            Console.WriteLine("Tryck på valfri knapp för att avsluta");
+            Console.ReadKey();
+        }
+    }
+}
